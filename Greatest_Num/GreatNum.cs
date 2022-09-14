7ae19d42165f4f0cc4c1ad04bdcb24a7be2e1177
Gaurav <Gaurav@LAPTOP-TREG306B>
2022-09-14 09:56:03 +0530

    Add project files.

---
+++ Greatest_Num/GreatNum.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greatest_Num
+{
+    internal class GreatNum
+    {
+        static void Main(string[] args)
+        { 
+
+        
+            int n1, n2,n3;
+
+        Console.WriteLine("Enter the value of n1:");
+            n1= int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the value of n2:");
+            n2= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of n3:");
+            n3 = int.Parse(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                if (n1 > n3)
+                {
+                    Console.WriteLine($"Largest Number is: {n1}");
+                }
+                else
+                {
+                    Console.WriteLine($"Largest Number is: {n3}");
+
+                }
+
+            }
+
+            else
+            {
+                if(n2> n3)
+                {
+                    Console.WriteLine($"Largest Number is: {n2}");
+                }
+                else
+                {
+                    Console.WriteLine($"Largest Number is: {n3}");
+                }
+            }
+            
+
+
+
+
+
+        }
+    }
+}
